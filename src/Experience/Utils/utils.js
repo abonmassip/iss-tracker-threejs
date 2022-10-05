2689c6ec98168b11646bf08956942a32f7634f06
Albert <abonmassip@gmail.com>
2022-10-05 08:45:46 -0700

    remove console log, fix API text

--- src/Experience/Utils/utils.js
+++ src/Experience/Utils/utils.js
@@ -15,7 +15,6 @@ export const fetchIssCoordinates = async () => {
   try {
     const response = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.log(error);
